b1fa26c8b1d8cb6a45c5df644d30722ebe3b220f
Ernest Prabhakar <ernest@quiltdata.io>
2023-01-28 15:19:07 -0800

    try to realign spock table

--- plugins/nf-quilt/src/test/nextflow/quilt/jep/QuiltParserTest.groovy
+++ plugins/nf-quilt/src/test/nextflow/quilt/jep/QuiltParserTest.groovy
@@ -67,13 +67,13 @@ class QuiltParserTest extends QuiltSpecification {
         parser.getTag() == tag
 
         where:
-        bare                                 | bucket   | query                      | pkg   | path | hash  | tag
-        'bucket'                             | 'bucket' | null                       | null  | ''   | null  | null
-        'BuCKet'                             | 'bucket' | null                       | null  | ''   | null  | null
-        'b#frag'                             | 'b'      | 'frag'                     | null  | ''   | null  | null
-        'B#package=q%2Fp'                    | 'b'      | 'package=q/p'              | 'q/p' | ''   | null  | null
-        'B#package=q%2Fp@hash'               | 'b'      | 'package=q/p@hash'         | 'q/p' | ''   | 'hash'| null
-        'B#package=q%2Fp:tag&path=a%2Fb'     | 'b'      | 'package=q/p:tag&path=a/b' | 'q/p' | 'a/b'| null  | 'tag'
+        bare                              | bucket   | query                      | pkg   | path   | hash   | tag
+        'bucket'                          | 'bucket' | null                       | null  | ''     | null   | null
+        'BuCKet'                          | 'bucket' | null                       | null  | ''     | null   | null
+        'b#frag'                          | 'b'      | 'frag'                     | null  | ''     | null   | null
+        'B#package=q%2Fp'                 | 'b'      | 'package=q/p'              | 'q/p' | ''     | null   | null
+        'B#package=q%2Fp@hash'            | 'b'      | 'package=q/p@hash'         | 'q/p' | ''     | 'hash' | null
+        'B#package=q%2Fp:tag&path=a%2Fb'  | 'b'      | 'package=q/p:tag&path=a/b' | 'q/p' | 'a/b'  | null   | 'tag'
     }
 
     void 'should parse package into components'() {
