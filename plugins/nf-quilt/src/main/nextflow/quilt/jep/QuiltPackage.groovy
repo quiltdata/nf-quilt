6575615b8851f5077c2a3299398c360a183ed280
Dima Ryazanov <dima@gmail.com>
2023-09-18 07:16:20 -0700

    Bump quiltcore and use workflows

--- plugins/nf-quilt/src/main/nextflow/quilt/jep/QuiltPackage.groovy
+++ plugins/nf-quilt/src/main/nextflow/quilt/jep/QuiltPackage.groovy
@@ -257,7 +257,7 @@ class QuiltPackage {
         Manifest m = builder.build()
 
         try {
-            m.push(namespace, "nf-quilt:${today()}-${msg}")
+            m.push(namespace, "nf-quilt:${today()}-${msg}", parsed.workflowName)
         } catch (IOException e) {
             return 1
         }
