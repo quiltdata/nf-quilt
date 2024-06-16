8652b3e8c7f00760cf389969b78de88e39372096
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2024-06-16 10:27:46 -0700

    reproduce null package error

--- plugins/nf-quilt/src/test/nextflow/quilt/nio/QuiltPathTest.groovy
+++ plugins/nf-quilt/src/test/nextflow/quilt/nio/QuiltPathTest.groovy
@@ -259,4 +259,12 @@ class QuiltPathTest extends QuiltSpecification {
         fullPath.toUriString().contains('?')
     }
 
+    void 'should return a valid package on relative paths'() {
+        when:
+        QuiltPath path = QuiltPathFactory.parse('quilt+s3://./')
+        then:
+        path.toString() == '.'
+        path.pkg() != null
+    }
+
 }
