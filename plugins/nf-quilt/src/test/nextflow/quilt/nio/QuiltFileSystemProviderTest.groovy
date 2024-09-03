aa2ac3e72e375cd2036092eaefe977127af7fb2d
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2024-09-03 14:36:20 -0700

    pass on windows
    Using QuiltPathFactory.parse

--- plugins/nf-quilt/src/test/nextflow/quilt/nio/QuiltFileSystemProviderTest.groovy
+++ plugins/nf-quilt/src/test/nextflow/quilt/nio/QuiltFileSystemProviderTest.groovy
@@ -4,12 +4,10 @@ package nextflow.quilt.nio
 import nextflow.quilt.QuiltSpecification
 import groovy.transform.CompileDynamic
 import java.nio.file.Path
-import java.nio.file.Paths
 import java.nio.file.Files
 import java.nio.file.CopyOption
 import java.nio.file.StandardCopyOption
 import groovy.util.logging.Slf4j
-import spock.lang.IgnoreIf
 
 /**
  *
@@ -30,12 +28,11 @@ class QuiltFileSystemProviderTest extends QuiltSpecification {
     // newDirectoryStream returns package path for write
     // do we need a new schema for quilt+local?
 
-    @IgnoreIf({ System.getProperty('os.name').contains('indows') })
     void 'should download file from remote to local destination'() {
         given:
         QuiltFileSystemProvider provider = new QuiltFileSystemProvider()
         String filename = 'README.md'
-        Path remoteFile = Paths.get('quilt+s3://quilt-example#package=examples%2fhurdat2&path=' + filename)
+        Path remoteFile = QuiltPathFactory.parse('quilt+s3://quilt-example#package=examples%2fhurdat2&path=' + filename)
         Path tempFolder = Files.createTempDirectory('quilt')
         Path tempFile = tempFolder.resolve(filename)
 
@@ -50,7 +47,7 @@ class QuiltFileSystemProviderTest extends QuiltSpecification {
     void 'should download folders from remote to local destination'() {
         given:
         QuiltFileSystemProvider provider = new QuiltFileSystemProvider()
-        Path remoteFolder = Paths.get('quilt+s3://quilt-example#package=examples%2fhurdat2')
+        Path remoteFolder = QuiltPathFactory.parse('quilt+s3://quilt-example#package=examples%2fhurdat2')
         Path tempFolder = Files.createTempDirectory('quilt')
         CopyOption opt = StandardCopyOption.REPLACE_EXISTING
         when:
